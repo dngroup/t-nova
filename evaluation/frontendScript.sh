61e5b8062767c352799a690e6423453062587fd8
dbourasseau <dbourasseau@viotech.net>
2016-04-27 20:08:24 +0200

    add evaluation of frontend

---
+++ evaluation/frontendScript.sh
@@ -0,0 +1,1 @@
+while true; do export amount=`rabbitmqctl list_queues  name messages | grep "celery" | head -1|sed -e 's/.*[^0-9]\([0-9]\+\)[^0-9]*$/\1/'`; echo $(date +%s),$((amount-old)),$amount;  export old=$amount; sleep 1; done
